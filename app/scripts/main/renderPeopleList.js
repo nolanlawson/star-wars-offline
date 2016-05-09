5bf0ec2537a51560c3cb86e963db0bc80ca3a179
Nolan Lawson <nolan.lawson@gmail.com>
2016-05-09 14:21:47 +0200

    add some images

--- app/scripts/main/renderPeopleList.js
+++ app/scripts/main/renderPeopleList.js
@@ -4,23 +4,31 @@ import people from './people.json';
 
 var peopleSection = document.querySelector('#people');
 
-function renderPeopleList() {
-
-  var card = `
+function renderPerson(person) {
+  return `
     <section class="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp">
-            <div class="mdl-card mdl-cell mdl-cell--9-col-desktop mdl-cell--6-col-tablet mdl-cell--4-col-phone">
+            <div class="mdl-card mdl-cell mdl-cell--9-col-desktop mdl-cell--4-col-tablet mdl-cell--4-col-phone">
               <div class="mdl-card__supporting-text">
-                <h4>Name of Character</h4>
-                This is where we'll talk about a particular character. Maybe provide some short info to grab
+                <h4>${person.name}</h4>
+                this is where we'll talk about a particular character. maybe provide some short info to grab
                 your attention.
               </div>
               <div class="mdl-card__actions">
-                <a href="#" class="mdl-button">This will do something cool</a>
+                <a href="#" class="mdl-button">learn more</a>
               </div>
             </div>
+            <div class="mdl-card mdl-cell mdl-cell--3-col-desktop mdl-cell--4-col-tablet mdl-cell--4-col-phone cover-image"
+              style="background-image: url('/images/people/person_${person.id}.png');">
+            </div>
           </section>`;
+}
 
-  peopleSection.innerHTML = card + card + card;
+function renderHTML() {
+  return people.map(renderPerson).join('\n');
+}
+
+function renderPeopleList() {
+  peopleSection.innerHTML = renderHTML();
 
 }
 
